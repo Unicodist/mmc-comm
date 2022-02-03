c6d0224e1f11bf599394dc157df9660429846f36
Unicodist <ashishneupane999@gmail.com>
2022-02-03 20:48:59 +0545

    :3

--- databaseinfo.py
+++ databaseinfo.py
@@ -1,9 +1,3 @@
-# sql_server = 'sql6.freesqldatabase.com'
-# sql_user = 'sql6468369'
-# sql_password = 'kEtMpRlsVs'
-# sql_database = 'sql6468369'
-
-
 sql_server = 'localhost'
 sql_user = 'root'
 sql_password = ''
